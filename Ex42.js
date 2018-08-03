2d850581fdf5aad420ead73cea43f941f59a0d46
Mareco Nabors <naborsmarico99@gmail.com>
2018-08-03 15:52:54 -0500

    review1

---
+++ Ex42.js
@@ -0,0 +1,19 @@
+/* 1)think of the key word use to put the array in reverse
+   2) realized that I need the ".reverse()" in the array
+   3) create the var with variable
+   4) print variable regularly
+   5) put variable in ".reverse()"
+   6) print variable
+
+*/
+function main() {
+
+}
+var squad = ["One", "Two","Three", "Four", "Five"];
+
+counter =4 ;
+while ([counter]>=0);{
+}
+console.log(squad[counter]);
+counter--;
+main();
---
+++ Ex43.js
@@ -0,0 +1,22 @@
+/*
+   1) create a function and end it with main
+   2)
+ */
+
+
+function main() {
+    var  randomNum = Math.floor(Math.random()*100) + 1)
+    var  
+
+}
+
+
+
+
+
+
+
+
+
+
+main()
